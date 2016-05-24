4f10249d67b0d0875b700edc58dfaa702d6b6464
Gilad Keinan <gilad.keinan@sap.com>
2016-05-24 18:27:42 +0300

    change db credentials

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     'localURL': 'mongodb://localhost/appname',
-    'remoteURL': 'mongodb://dbuser:dbuser@ds035975.mongolab.com:35975/security-booth-db'
+    'remoteURL': 'mongodb://gali123:gilad123@ds035975.mongolab.com:35975/security-booth-db'
 };
